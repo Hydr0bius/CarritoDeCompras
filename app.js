024f137cd727db9e9d1e45ae437cff1d2b62adb8
Ricardo Polanco <polancoblancoricardo@gmail.com>
2023-04-09 17:55:24 -0600

    :construction: Leer los datos del curso que seleccionamos

--- app.js
+++ app.js
@@ -14,6 +14,22 @@ function cargarEventListeners(){
 function agregarCurso(e){
     e.preventDefault();
     if (e.target.classList.contains('agregar-carrito')){
-        console.log('agregando al carrito....');
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+        leerDatosCurso(cursoSeleccionado);
     }
+};
+
+//leer el contenido del HTML y extrae la informacion del curso
+function leerDatosCurso(curso){
+    console.log(curso);
+
+    //crear un objeto con el contenido del curso actual
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1,
+    };
+    console.log(infoCurso);
 };
